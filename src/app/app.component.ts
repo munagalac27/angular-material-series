fb11c84389fdbd7da0a0ef8d8fdd5ad333447a65
chaithanyamunagala <Chaithanya.Munagala@davaco.com>
2025-03-08 13:28:09 -0500

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang-side-navigation';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  closeSidenav() {
+    this.sidenav.close();
+  }
 }
